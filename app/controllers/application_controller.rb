fd8bc9e0c308221085e85d965352015665b28bb4
Theo Weller <weller.theodor@gmail.com>
2019-04-02 18:57:39 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  	# binding.pry
+  	# erb(:index)
   end
+
+  get '/articles/new' do
+  	erb(:new)
+  	
+  end
+
+  get '/articles' do
+  	# binding.pry
+  	@article = Article.all
+  	erb(:index)
+  end
+
+  post '/articles' do
+  	Article.create(params)
+  	# @article = Article.last.id
+  	redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles/:id' do
+  	# binding.pry
+  	@article = Article.find(params["id"])
+  	erb(:show)
+  end
+
+  get '/articles/:id/edit' do
+  	# binding.pry
+  	erb(:edit)
+  end
+
+  patch '/articles/:id' do 
+  	# binding.pry
+  	@article = Article.find(params["id"])
+  	@article.update(id: params["id"], title: params["title"], content: params["content"])
+  	redirect "/articles/#{@article.id}"
+  end
+
+
 end
+  
+
--- config.ru
+++ config.ru
@@ -1,13 +1,16 @@
-
 require './config/environment'
-
-begin
-  fi_check_migration
 
   use Rack::MethodOverride
 
   run ApplicationController
-rescue ActiveRecord::PendingMigrationError => err
-  STDERR.puts err
-  exit 1
-end
+  
+# begin
+#   fi_check_migration
+
+#   use Rack::MethodOverride
+
+#   run ApplicationController
+# rescue ActiveRecord::PendingMigrationError => err
+#   STDERR.puts err
+#   exit 1
+# end
